746fa80163e741cf92538f1a5c08e5b964b8e466
KotovSergeyV <75029707+KotovSergeyV@users.noreply.github.com>
2024-01-16 18:25:43 +0300

    FirstPart

---
+++ Labirynth/Assets/Shealth.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class Shealth : XRBaseInteractable
+{
+    [Header("ShealthAdds")]
+    [SerializeField] private GameObject _object;
+    [SerializeField] private string _objTag;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
