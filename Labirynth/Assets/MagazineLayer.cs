f732ad21be2dcb6a20cd8da2aad16fec9875b3af
KotovSergeyV <75029707+KotovSergeyV@users.noreply.github.com>
2024-01-21 23:13:29 +0300

    Doors, card, magazine

---
+++ Labirynth/Assets/MagazineLayer.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class MagazineLayer : MonoBehaviour
+{
+    public void ChangeLayerOff()
+    {
+        var x = gameObject.GetComponent<XRSocketInteractorByTag>().GetOldestInteractableSelected();
+        x.transform.gameObject.GetComponent<Magazine>().IntLayerOff();
+    }
+    public void ChangeLayerOn()
+    {
+        var x = gameObject.GetComponent<XRSocketInteractorByTag>().GetOldestInteractableSelected();
+        x.transform.gameObject.GetComponent<Magazine>().IntLayerOn();
+    }
+}
+
