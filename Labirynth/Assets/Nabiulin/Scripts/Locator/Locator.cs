a0284848a3d6a71bda6f2c916a33affe8013de8e
El_Nico <118479527+NicoRoso@users.noreply.github.com>
2024-01-23 00:15:52 +0300

    Locator is Ready

---
+++ Labirynth/Assets/Nabiulin/Scripts/Locator/Locator.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Locator : MonoBehaviour
+{
+    public string finishTag = "Finish";
+    public string keycardTag = "Keycard";
+    [SerializeField] private Transform target;
+    private NavMeshPath path;
+
+    [SerializeField] private AudioSource _source;
+    [SerializeField] private AudioClip _locatorOnClip;
+    [SerializeField] private AudioClip _locatorErrorClip;
+
+    void Start()
+    {
+        _source = GetComponent<AudioSource>();
+        target = FindTargetWithTag(keycardTag);
+        path = new NavMeshPath();
+        DrawPath();
+        this.enabled = false;
+    }
+
+    void Update()
+    {
+        DrawPath();
+
+        if (target != null && target.CompareTag(keycardTag) && Vector3.Distance(transform.position, target.position) < 1.0f)
+        {
+            target = FindTargetWithTag(finishTag);
+            DrawPath();
+        }
+    }
+
+    void DrawPath()
+    {
+        if (target != null)
+        {
+            NavMesh.CalculatePath(transform.position, target.position, NavMesh.AllAreas, path);
+            LineRenderer lineRenderer = GetComponent<LineRenderer>();
+            lineRenderer.positionCount = path.corners.Length;
+            lineRenderer.SetPositions(path.corners);
+        }
+
+        if (path.corners.Length <= 0)
+        {
+            this.enabled = false;
+        }
+    }
+
+    private void OnEnable()
+    {
+        _source.PlayOneShot(_locatorOnClip);
+        LineRenderer lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.enabled = true;
+    }
+
+    private void OnDisable()
+    {
+        if (path.corners.Length <= 0)
+        {
+            _source.PlayOneShot(_locatorErrorClip);
+        }
+        else
+        {
+            _source.PlayOneShot(_locatorOnClip);
+        }
+        LineRenderer lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.enabled = false;
+    }
+
+    Transform FindTargetWithTag(string tag)
+    {
+        return GameObject.FindGameObjectWithTag(tag)?.transform;
+    }
+}
